711487b2fcc05cda57fd77d9971f66f0f01961e0
fredirk <fredr@DESKTOP-TFU7VNK>
2021-03-01 15:42:57 +0100

    Add OrderModel

---
+++ ElectroShop/Models/OrderModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectroShop.Models
+{
+    public class OrderModel
+    {
+        public int OrderId { get; set; }
+
+        public List<ProductModel> Products { get; set; } = new List<ProductModel>();
+    }
+}
