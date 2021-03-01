e45ac6070744a96eea5dc8c3c893b604a29eb974
fredirk <fredr@DESKTOP-TFU7VNK>
2021-03-01 14:44:33 +0100

    Add CategoryModel and ProductModel

---
+++ ElectroShop/Models/CategoryModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectroShop.Models
+{
+    public class CategoryModel
+    {
+        public int CategoryId { get; set; }
+
+        [MaxLength(125)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public CategoryModel ParentCategory { get; set; }
+        public List<CategoryModel> SubCategories { get; set; } = new List<CategoryModel>();
+        public List<ProductModel> Products { get; set; } = new List<ProductModel>();
+    }
+}
