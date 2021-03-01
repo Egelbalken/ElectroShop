5eb5f3bae0524ad8f31d954f3aab77804e0e6a00
kent Jakobsson <kentegel@gmail.com>
2021-03-01 10:44:38 +0100

    Initial commit

---
+++ ElectroShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElectroShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
