c7a18411c4c7515f44b3f92e9a9ee548fdfda846
kent Jakobsson <kentegel@gmail.com>
2021-03-02 10:51:59 +0100

    error.css update bg color

--- ElectroShop/Controllers/HomeController.cs
+++ ElectroShop/Controllers/HomeController.cs
@@ -33,10 +33,16 @@ namespace ElectroShop.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        public IActionResult Error()
+        {
+            return RedirectToAction("Error");
         }
     }
 }
