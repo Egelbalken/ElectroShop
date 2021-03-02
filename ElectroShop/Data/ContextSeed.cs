b13eea7db6ed21037750be356830c4e080d0de4e
Tornehammar <bardia.tornehammar@gmail.com>
2021-03-02 11:02:45 +0100

    Database Identity Admin & Customer Ready, New User = Customer. Seeded

---
+++ ElectroShop/Data/ContextSeed.cs
@@ -0,0 +1,41 @@
+﻿using ElectroShop.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectroShop.Data
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Customer.ToString()));
+        }
+        public static async Task SeedAdminAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "SuperBardia",
+                Email = "SuperBardia@gmail.com",
+                FirstName = "Super",
+                LastName = "Duper",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "Super123#");
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Admin.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Customer.ToString());
+                }
+
+            }
+        }
+    }
+}
