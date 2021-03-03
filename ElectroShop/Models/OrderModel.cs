d01949e050c9e1ac1463b2d7d947a253d9282534
fredirk <fredrik.kaspersen@outlook.com>
2021-03-03 13:23:19 +0100

    Update OrderModel

--- ElectroShop/Models/OrderModel.cs
+++ ElectroShop/Models/OrderModel.cs
@@ -11,9 +11,7 @@ namespace ElectroShop.Models
         [Key]
         public int OrderId { get; set; }
 
-        //public int CustomerId { get; set; }
         public ApplicationUser Customer { get; set; }
-
         public List<OrderDetailModel> OrderDetails { get; set; } = new List<OrderDetailModel>();
 
         public decimal TotalCost => OrderDetails.Select(order => order.Product.Price * order.Quantity).Sum();
