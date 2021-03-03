c16aa2580fe6c9491434e0bf6b8348b5421d5640
kent Jakobsson <kentegel@gmail.com>
2021-03-03 11:38:50 +0100

    carousel
    _partielView added carousel

--- ElectroShop/Controllers/HomeController.cs
+++ ElectroShop/Controllers/HomeController.cs
@@ -37,8 +37,6 @@ namespace ElectroShop.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-
-
             return View("ErrorController.PageNotFound");
             //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
