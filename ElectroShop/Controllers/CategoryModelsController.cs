53562fcdf52c1ce799cb5446774ffb9d22433bee
kent Jakobsson <kentegel@gmail.com>
2021-03-03 14:32:11 +0100

    Scaffolding CategoryModelController

---
+++ ElectroShop/Controllers/CategoryModelsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ElectroShop.Data;
+using ElectroShop.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ElectroShop.Controllers
+{
+    public class CategoryModelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryModelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CategoryModels
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Categories.Include(c => c.ParentCategory);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: CategoryModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoryModel = await _context.Categories
+                .Include(c => c.ParentCategory)
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (categoryModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryModel);
+        }
+
+        // GET: CategoryModels/Create
+        public IActionResult Create()
+        {
+            ViewData["ParentCategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
+            return View();
+        }
+
+        // POST: CategoryModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CategoryId,Name,Description,ParentCategoryId")] CategoryModel categoryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categoryModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParentCategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", categoryModel.ParentCategoryId);
+            return View(categoryModel);
+        }
+
+        // GET: CategoryModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoryModel = await _context.Categories.FindAsync(id);
+            if (categoryModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["ParentCategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", categoryModel.ParentCategoryId);
+            return View(categoryModel);
+        }
+
+        // POST: CategoryModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CategoryId,Name,Description,ParentCategoryId")] CategoryModel categoryModel)
+        {
+            if (id != categoryModel.CategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categoryModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryModelExists(categoryModel.CategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParentCategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", categoryModel.ParentCategoryId);
+            return View(categoryModel);
+        }
+
+        // GET: CategoryModels/Delete/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoryModel = await _context.Categories
+                .Include(c => c.ParentCategory)
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (categoryModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryModel);
+        }
+
+        // POST: CategoryModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [Authorize(Roles = "Admin")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var categoryModel = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(categoryModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryModelExists(int id)
+        {
+            return _context.Categories.Any(e => e.CategoryId == id);
+        }
+    }
+}
