daed60a05a540c220cc42206ec26ca2448d7723a
kent Jakobsson <kentegel@gmail.com>
2021-03-03 14:02:16 +0100

    IRepositrys added

---
+++ ElectroShop/Models/ICategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectroShop.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElectroShop.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<CategoryModel> AllCategories { get; }
+
+        CategoryModel GetCategory(int id);
+
+    }
+
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public CategoryRepository(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public IEnumerable<CategoryModel> AllCategories => throw new NotImplementedException();
+
+        public CategoryModel GetCategory(int id)
+        {
+            return applicationDbContext.Categories
+                .Include(category => category.SubCategories)
+                .Include(category => category.ParentCategory)
+                .Include(category => category.Products)
+                .FirstOrDefault(category => category.CategoryId == id);
+        }
+    }
+}
---
+++ ElectroShop/Models/IProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using ElectroShop.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectroShop.Models
+{
+    public interface IProductRepository
+    {
+        IEnumerable<ProductModel> AllProducts { get; }
+
+        IEnumerable<ProductModel> OnSaleProduct { get; }
+
+        ProductModel GetProduct(int productId);
+    }
+
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public ProductRepository(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public IEnumerable<CategoryModel> AllCategories => applicationDbContext.Categories.ToList();
+
+        public IEnumerable<ProductModel> AllProducts => throw new NotImplementedException();
+
+        public IEnumerable<ProductModel> OnSaleProduct => throw new NotImplementedException();
+
+        public ProductModel GetProduct(int id)
+        {
+            return applicationDbContext.Products
+                .Include(product => product.Category)
+                .FirstOrDefault(product => product.ProductId == id);
+        }
+
+        //public ProductModel GetProduct(int productId)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
+
+
