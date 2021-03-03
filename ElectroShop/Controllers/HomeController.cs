fbefa2ae7e96167bd39b2d66636d37d8b4ef0156
kent Jakobsson <kentegel@gmail.com>
2021-03-03 12:24:59 +0100

    Updated carousell

--- ElectroShop/Controllers/HomeController.cs
+++ ElectroShop/Controllers/HomeController.cs
@@ -18,8 +18,9 @@ namespace ElectroShop.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int id)
         {
+
             return View();
         }
 
@@ -37,7 +38,7 @@ namespace ElectroShop.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return RedirectToAction("PageNotFound", "Error");
+            return RedirectToAction("PageNotFound", "ErrorController");
             //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
