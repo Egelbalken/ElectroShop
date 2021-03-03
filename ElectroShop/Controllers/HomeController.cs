f01d458b04e9dfe6ba444fe8f23b81d11b588f67
kent Jakobsson <kentegel@gmail.com>
2021-03-03 11:48:34 +0100

    Error
    Error

--- ElectroShop/Controllers/HomeController.cs
+++ ElectroShop/Controllers/HomeController.cs
@@ -37,7 +37,7 @@ namespace ElectroShop.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View("ErrorController.PageNotFound");
+            return RedirectToAction("PageNotFound", "Error");
             //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
