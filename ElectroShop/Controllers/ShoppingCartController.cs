5de4ceb070fa02fd3da73767812e727360830f6b
kent Jakobsson <kentegel@gmail.com>
2021-03-05 08:49:06 +0100

    Model Binding on cart
    Model Binding on cart
    + added "add to cart in product details"

--- ElectroShop/Controllers/ShoppingCartController.cs
+++ ElectroShop/Controllers/ShoppingCartController.cs
@@ -5,9 +5,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ElectroShop.Controllers
 {
+    [Authorize(Roles = "Customer")]
     public class ShoppingCartController : Controller
     {
         private readonly IProductRepository _productRepository;
