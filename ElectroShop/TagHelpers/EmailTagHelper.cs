49611a59973622994036d33b66b7513de77ae32d
kent Jakobsson <kentegel@gmail.com>
2021-03-08 09:08:50 +0100

    email
    email taghelpers, contact link added.

---
+++ ElectroShop/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectroShop.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Address { get; set; }
+
+        public string Content { get; set; }
+
+        // taghelper to send email to conntact ElectroShop
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Address);
+            output.Content.SetContent(Content);
+        }
+    }
+}
+            
