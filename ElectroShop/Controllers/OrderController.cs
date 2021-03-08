e29934cfb9f564dea0080556253ea70a053c1bdb
fredirk <fredrik.kaspersen@outlook.com>
2021-03-08 12:10:45 +0100

    Add Checkout controller

---
+++ ElectroShop/Controllers/OrderController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ElectroShop.Models;
+using ElectroShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectroShop.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace ElectroShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ShoppingCart _shoppingCart;
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public OrderController(ShoppingCart shoppingCart,
+            ApplicationDbContext applicationDbContext,
+            UserManager<ApplicationUser> userManager)
+        {
+            _shoppingCart = shoppingCart;
+            _applicationDbContext = applicationDbContext;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            var checkoutViewModel = new CheckoutViewModel
+            {
+                ShoppingCartItems = _shoppingCart.GetShoppingCartItems()
+            };
+
+            return View(checkoutViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(CheckoutViewModel checkoutViewModel)
+        {
+            var userId = _userManager.GetUserId(User);
+
+            var newOrder = new OrderModel
+            {
+                Customer = _applicationDbContext.ApplicationUsers.Find(userId),
+                OrderDetails = _shoppingCart.GetShoppingCartItems()
+                    .Select(cartItem => new OrderDetailModel
+                    {
+                        Product = cartItem.product,
+                        Quantity = cartItem.Amount,
+                    }).ToList()
+            };
+
+            _applicationDbContext.Orders.Add(newOrder);
+            _applicationDbContext.SaveChanges();
+
+            return RedirectToAction();
+        }
+    }
+}
