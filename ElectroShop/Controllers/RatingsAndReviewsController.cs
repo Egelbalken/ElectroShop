1bd562ba026e9fef98cd3bfeb99c8e30d327300b
kent Jakobsson <kentegel@gmail.com>
2021-03-17 11:38:41 +0100

    javascript implementation of rateings

---
+++ ElectroShop/Controllers/RatingsAndReviewsController.cs
@@ -0,0 +1,44 @@
+﻿using ElectroShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectroShop.Controllers
+{
+    public class RatingsAndReviewsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public RatingsAndReviewsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult StarRatings() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult StarRatings(int starRatings)
+        {
+            var stars = _productRepository;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TextReview()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TextReview(string textReview)
+        {
+            return View();
+        }
+    }
+}
