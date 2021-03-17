646521365ffd82b23b1f2239ce151c2a608286e5
kent Jakobsson <kentegel@gmail.com>
2021-03-17 08:22:55 +0100

    Added TextAndRatingComponents

---
+++ ElectroShop/Component/StarRatingViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using ElectroShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectroShop.Component
+{
+    public class StarRatingViewComponent : ViewComponent
+    {
+        private readonly ProductRatingModel _productRating;
+
+        public StarRatingViewComponent(ProductRatingModel productRating)
+        {
+            _productRating = productRating;
+        }
+
+        public IViewComponentResult InvokeAsync()
+        {
+            var stars = _productRating;
+
+            return View();
+        }
+
+    }
+}
---
+++ ElectroShop/Component/TextReviewViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using ElectroShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectroShop.Component
+{
+    public class TextReviewViewComponent : ViewComponent
+    {
+        private readonly ProductReviewModel _productReview;
+
+        public TextReviewViewComponent(ProductReviewModel productReview)
+        {
+            _productReview = productReview;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
