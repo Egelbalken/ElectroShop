7e056d8fe28c34a6da1b43394275eac60dbd59da
fredirk <fredrik.kaspersen@outlook.com>
2021-03-18 09:47:53 +0100

    Add RateReviewModel

---
+++ ElectroShop/ViewModels/RateReviewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectroShop.ViewModels
+{
+    public class RateReviewViewModel
+    {
+        [Required]
+        public int ProductId { get; set; } 
+
+        [Required]
+        [Range(1, 5)]
+        public int Rate { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string Review { get; set; }
+    }
+}
